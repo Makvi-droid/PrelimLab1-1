ebc5bb2d0a9c8a3c17c0262562ce959a21d7f21f
Makvi-droid <pasbenauro05@gmail.com>
2024-09-21 01:11:16 +0800

    finishing

--- exam/nested-loop/reverse-right.js
+++ exam/nested-loop/reverse-right.js
@@ -1,6 +1,6 @@
-for(let z = 1; z <= 1; z++){
+for(let z = 1; z <= 5; z++){
     let line = '';
-    for(let x = 1; x <= 5 - 1; x++){
+    for(let x = 1; x <= 6 - z; x++){
         line += '*';
     }
     document.write(`${line} ${'<br>'}`);
